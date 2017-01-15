0c28e5f719074fbecd4656e7df875bcc9f8aa3ee
Steve Whitmire <swhit114@gmail.com>
2017-01-15 13:41:24 -0500

    Created and added PixelColor and JUnit4 tests.

---
+++ MouseRecorder/src/main/java/com/github/stevewhit/mouserecorder/monitor/PixelColor.java
@@ -0,0 +1,109 @@
+package com.github.stevewhit.mouserecorder.monitor;
+
+/**
+ * The RGB Color of any given pixel.
+ * @author Steve Whitmire (swhit114@gmail.com)
+ *
+ */
+public class PixelColor
+{
+	/**
+	 * The Red, Green, and Blue value of the color represented as a single integer.
+	 */
+	private int rgbValue;
+	
+	/**
+	 * Constructor that accepts an Red, Green, Blue integer
+	 * @param rgbValue The RBG Value of this color.
+	 */
+	public PixelColor(int rgbValue)
+	{
+		setRGBValue(rgbValue);
+	}
+	
+	/**
+	 * Returns the Red, Green, and Blue value for this color.
+	 * @return Returns the RGB Value for this color as an integer.
+	 */
+	public int getRGBValue()
+	{
+		return this.rgbValue;
+	}
+	
+	/**
+	 * Returns the Red component of this color.
+	 * @return Returns the Red component of this color as an integer.
+	 */
+	public int getRedValue()
+	{
+		return (this.rgbValue >> 16) & 0xFF;
+	}
+	
+	/**
+	 * Returns the Green component of this color.
+	 * @return Returns the Green component of this color as an integer.
+	 */
+	public int getGreenValue()
+	{
+		return (this.rgbValue >> 8) & 0xFF;
+	}
+	
+	/**
+	 * Returns the Blue component of this color.
+	 * @return Returns the Blue component of this color as an integer.
+	 */
+	public int getBlueValue()
+	{
+		return this.rgbValue & 0xFF;
+	}
+
+	/**
+	 * Updates the RGB Value for this color. 
+	 * @param argbValue The Red, Green, and Blue values for this color.
+	 */
+	private void setRGBValue(int rgbValue)
+	{
+		this.rgbValue = rgbValue;
+	}
+	
+	/**
+	 * Returns a formatted string indicating the red, green, and blue components of this color.
+	 * <pre>
+	 * Example output ==> Red: 0, Green: 233, Blue: 213
+	 * </pre>
+	 */
+	@Override
+	public String toString()
+	{
+		return String.format("Red: %1$d, Green: %2$d, Blue: %3$d", getRedValue(), getGreenValue(), getBlueValue());
+	}
+	
+	/**
+	 * Checks for comparison of two PixelColors. Returns true if they're equal, otherwise false.
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		// Make sure it isn't null.
+		if (obj == null)
+		{
+			return false;
+		}
+		
+		// Make sure they're relatable by class structure.
+		if (!PixelColor.class.isAssignableFrom(obj.getClass()))
+		{
+			return false;
+		}
+		
+		final PixelColor castObj = (PixelColor) obj;
+		
+		// Compare the RGB value. This includes comparison of the individual Red, Green, and Blue values.
+		if (castObj.getRGBValue() != this.getRGBValue())
+		{
+			return false;
+		}
+		
+		return true;
+	}
+}
---
+++ MouseRecorder/src/test/java/com/github/stevewhit/mouserecorder/monitor/PixelColorTest.java
@@ -0,0 +1,74 @@
+package com.github.stevewhit.mouserecorder.monitor;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PixelColorTest
+{
+	PixelColor color;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		color = new PixelColor(11691775); // HEX: #B266FF
+	}
+	
+	@After
+	public void tearDown() throws Exception
+	{
+		color = null;
+	}
+	
+	//====================================================================
+	
+	@Test
+	public void testPixelColor()
+	{
+		assertTrue(color.getRGBValue() == 11691775);
+	}
+	
+	@Test
+	public void testGetRGBValue()
+	{
+		assertTrue(color.getRGBValue() == 11691775);
+	}
+	
+	@Test
+	public void testGetRedValue()
+	{
+		assertTrue(color.getRedValue() == 178);
+	}
+	
+	@Test
+	public void testGetGreenValue()
+	{
+		assertTrue(color.getGreenValue() == 102);
+	}
+	
+	@Test
+	public void testGetBlueValue()
+	{
+		assertTrue(color.getBlueValue() == 255);
+	}
+	
+	@Test
+	public void testToString()
+	{
+		assertEquals(color.toString(), "Red: 178, Green: 102, Blue: 255");
+	}
+	
+	@Test
+	public void testEqualsObject()
+	{
+		assertFalse(color.equals(null));
+		assertFalse(color.equals(new String("shouldn't equal")));
+		assertFalse(color.equals(new PixelColor(11691774)));
+		assertTrue(color.equals(new PixelColor(11691775)));
+	}
+	
+}
