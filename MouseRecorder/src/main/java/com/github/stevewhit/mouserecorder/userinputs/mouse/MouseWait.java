01217943ec636192d3ea2b18075745ec09e0902f
Steve Whitmire <swhit114@gmail.com>
2017-01-17 14:26:39 -0500

    Added mouse wait class and JUnit4 tests.

---
+++ MouseRecorder/src/main/java/com/github/stevewhit/mouserecorder/userinputs/mouse/MouseWait.java
@@ -0,0 +1,77 @@
+package com.github.stevewhit.mouserecorder.userinputs.mouse;
+
+import java.time.LocalDateTime;
+
+import com.github.stevewhit.mouserecorder.userinputs.AbstractInputAction;
+
+public class MouseWait extends AbstractInputAction
+{
+	/**
+	 * Wait time in milliseconds
+	 */
+	private long waitTime;
+	
+	/**
+	 * Main constructor for the class that initializes the wait time.
+	 * @param waitTime Time in milliseconds. 
+	 */
+	public MouseWait(long waitTime) throws IllegalArgumentException
+	{
+		super(LocalDateTime.now());
+		
+		try
+		{
+			setWaitTime(waitTime);
+		}
+		catch (IllegalArgumentException ex) 
+		{
+			waitTime = -1;
+			
+			throw new IllegalArgumentException("Could not create mouse wait action because ==> " + ex.getMessage());
+		}
+	}
+	
+	/**
+	 * Returns the wait time associated with this mouse wait action.
+	 * @return Returns the wait time in milliseconds.
+	 */
+	public long getWaitTime()
+	{
+		return this.waitTime;
+	}
+	
+	/**
+	 * Checks this action for validity by checking to make sure the wait time is >= 0.
+	 * @return Returns true if this wait action is valid, otherwise false.
+	 */
+	public boolean isValidWait()
+	{
+		return waitTime >= 0;
+	}
+	
+	/**
+	 * Returns a formatted string that represents this mouse wait action.
+	 * <pre>
+	 * Example output: 'Wait: 12343ms'
+	 * </pre>
+	 */
+	@Override
+	public String toString()
+	{
+		return String.format("Wait: %1$dms", waitTime);
+	}
+	
+	/**
+	 * Updates the wait time for this Mousewait action.
+	 * @param waitTime The amount of time to wait in milliseconds.
+	 */
+	private void setWaitTime(long waitTime)
+	{
+		if (waitTime < 0)
+		{
+			throw new IllegalArgumentException("Wait time cannot be less than 0.");
+		}
+		
+		this.waitTime = waitTime;
+	}
+}
---
+++ MouseRecorder/src/test/java/com/github/stevewhit/mouserecorder/userinputs/mouse/MouseWaitTest.java
@@ -0,0 +1,69 @@
+package com.github.stevewhit.mouserecorder.userinputs.mouse;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MouseWaitTest
+{
+	MouseWait wait;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		wait = new MouseWait(12344321);
+	}
+	
+	@After
+	public void tearDown() throws Exception
+	{
+		wait = null;
+	}
+	
+	//================================================================
+	
+	@Test
+	public void testToString()
+	{
+		assertEquals("Wait: 12344321ms", wait.toString());
+	}
+
+	//================================================================
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testMouseWait_NegativeWait()
+	{
+		wait = new MouseWait(-1);
+	}
+
+	@Test
+	public void testMouseWait_valid()
+	{
+		assertTrue(wait.isValidWait());
+		assertTrue(wait.getWaitTime() == 12344321);
+		
+		wait = new MouseWait(0);
+		wait = new MouseWait(1);
+	}
+	
+	//================================================================
+	
+	@Test
+	public void testGetWaitTime()
+	{
+		assertTrue(wait.getWaitTime() == 12344321);
+	}
+
+	//================================================================
+	
+	@Test
+	public void testIsValidWait()
+	{
+		assertTrue(wait.isValidWait());
+	}
+
+	//================================================================
+	
+}
