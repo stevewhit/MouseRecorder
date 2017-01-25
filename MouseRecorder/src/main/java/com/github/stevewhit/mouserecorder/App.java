daeb78c67f38dd5221239202528151a322a2ef73
Steve Whitmire <swhit114@gmail.com>
2017-01-25 16:48:39 -0500

    Test commit.

--- MouseRecorder/src/main/java/com/github/stevewhit/mouserecorder/App.java
+++ MouseRecorder/src/main/java/com/github/stevewhit/mouserecorder/App.java
@@ -1,13 +1,20 @@
 package com.github.stevewhit.mouserecorder;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import com.github.stevewhit.mouserecorder.userinputs.keyboard.KeyboardKeyPress;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws AWTException
     {
         System.out.println( "Hello World!" );
+                
+        
+        System.out.println("Done..");
     }
 }
