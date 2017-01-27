134bbdff15954d1ccd966850766cbb3c98bab3d5
Steve Whitmire <swhit114@gmail.com>
2017-01-27 17:30:16 -0500

    Added GlobalMouseWheelTracker for future development. Not going to implement now.

---
+++ MouseRecorder/src/main/java/com/github/stevewhit/mouserecorder/inputtracking/GlobalMouseWheelTracker.java
@@ -0,0 +1,15 @@
+package com.github.stevewhit.mouserecorder.inputtracking;
+
+import org.jnativehook.mouse.NativeMouseWheelEvent;
+import org.jnativehook.mouse.NativeMouseWheelListener;
+
+public class GlobalMouseWheelTracker implements NativeMouseWheelListener
+{
+
+	@Override
+	public void nativeMouseWheelMoved(NativeMouseWheelEvent arg0)
+	{
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+	
+}
