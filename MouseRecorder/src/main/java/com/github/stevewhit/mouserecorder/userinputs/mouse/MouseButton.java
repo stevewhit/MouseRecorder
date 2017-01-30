24255495d476500412cfc6f3fd807516d0d31b38
Steve Whitmire <swhit114@gmail.com>
2017-01-30 09:19:46 -0500

    Added integer values to mouse buttons for playback.

--- MouseRecorder/src/main/java/com/github/stevewhit/mouserecorder/userinputs/mouse/MouseButton.java
+++ MouseRecorder/src/main/java/com/github/stevewhit/mouserecorder/userinputs/mouse/MouseButton.java
@@ -1,8 +1,22 @@
 package com.github.stevewhit.mouserecorder.userinputs.mouse;
 
+import java.awt.event.InputEvent;
+
 public enum MouseButton
 {
-	Left,
-	Right,
-	ScrollWheel
+	Left(InputEvent.BUTTON1_MASK),
+	Right(InputEvent.BUTTON2_MASK ),
+	ScrollWheel(InputEvent.BUTTON3_MASK );
+	
+	private int buttonNum;
+	
+	private MouseButton(int buttonNum)
+	{
+		this.buttonNum = buttonNum;
+	}
+	
+	public int getButtonNum()
+	{
+		return buttonNum;
+	}	
 }
