d3cd2603d82b5ed5c720e29d626f4094067d3768
Steve Whitmire <swhit114@gmail.com>
2017-02-16 18:36:40 -0500

    Added 'main' to the project so it can be ran.

--- MouseRecorder/src/main/java/com/github/stevewhit/mouserecorder/App.java
+++ MouseRecorder/src/main/java/com/github/stevewhit/mouserecorder/App.java
@@ -1,21 +1,35 @@
 package com.github.stevewhit.mouserecorder;
 
 import java.awt.AWTException;
-import java.awt.Robot;
-import com.github.stevewhit.mouserecorder.userinputs.keyboard.KeyboardKeyPress;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import com.github.stevewhit.mouserecorder.ui.MouseRecorderGUI;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args ) throws AWTException
+    public static void main( String[] args ) throws AWTException, IOException, IllegalArgumentException, DataFormatException, InterruptedException
     {
-        System.out.println( "Hello World!" );
-                
-        // TEST
-        
-        System.out.println("Done..");
+        SwingUtilities.invokeLater(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				com.jtattoo.plaf.acryl.AcrylLookAndFeel.setTheme("Black", "INSERT YOUR LICENSE KEY HERE", "");
+		        
+		        // select the Look and Feel
+		        try
+				{
+					UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+
+		        new MouseRecorderGUI();
+			}
+		});
     }
 }
